25c2c30a47333da588b5954f907eace4694fa9ee
frdrk00 <masterkooo@gmail.com>
2023-04-26 23:29:57 +0200

    created: nextAuth (google) & mongoDB

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
 export default function Home() {
-  return (
-    <div className='bg-red-500'>
-      <h1>Hello World</h1>
+  const { data: session } = useSession()
+  if (!session) {
+      return (
+    <div className="bg-gray-800 w-screen h-screen flex items-center">
+      <div className="text-center w-full">
+        <button onClick={() => signIn('google')} className="bg-white px-4 p-2 rounded-lg">Login with Google</button>
+      </div>
     </div>
+  )
+
+  }
+  return (
+    <div>logged in {session.user.email}</div>
   )
 }
