aee87de0881728436696c727020574b74afdfd4b
frdrk00 <masterkooo@gmail.com>
2023-04-27 18:46:51 +0200

    basic design

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,28 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 
 export default function Home() {
-  const { data: session } = useSession()
-  if (!session) {
-      return (
-    <div className="bg-gray-800 w-screen h-screen flex items-center">
-      <div className="text-center w-full">
-        <button onClick={() => signIn('google')} className="bg-white px-4 p-2 rounded-lg">Login with Google</button>
+  const {data: session} = useSession()
+
+  return <Layout>
+    <div className="text-gray-800 flex justify-between">
+      <h2>
+        Hello, <b>{session?.user?.name}</b>
+      </h2>
+      <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+        <Image
+          onClick={() => signOut()}
+          src={session?.user?.image}
+          width={28}
+          height={28}
+          alt="Profile pic"
+          className="cursor-pointer mx-auto  hover:opacity-50"
+        />
+        <span className="px-2">
+          {session?.user?.name}
+        </span>
       </div>
     </div>
-  )
-
-  }
-  return (
-    <div>logged in {session.user.email}</div>
-  )
+  </Layout>
 }
