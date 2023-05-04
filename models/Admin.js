662adefd3e6624deff8df298ed2e4e7250e37ea7
frdrk00 <masterkooo@gmail.com>
2023-05-04 21:55:50 +0200

    admin auth fix

--- models/Admin.js
+++ models/Admin.js
@@ -1,7 +1,7 @@
 import {model, models, Schema} from "mongoose";
 
 const adminSchema = new Schema({
-    email: {type: String, required: true, unique: true},
-}, {timestamps: true})
+  email: {type: String, required: true, unique: true},
+}, {timestamps: true});
 
-export const Admin = models?.Admin || model('Admin', adminSchema)
+export const Admin = models?.Admin || model('Admin', adminSchema);
